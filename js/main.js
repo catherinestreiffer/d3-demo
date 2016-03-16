cf463e8876d99b2f255cf0ebeba36eb092c942d9
catherinestreiffer <catherinestreiffer@gmail.com>
2016-03-16 12:11:32 -0500

    done up through module 7 example 2.8

---
+++ js/main.js
@@ -0,0 +1,70 @@
+//execute script when window is loaded
+window.onload = function(){
+  //SVG dimension variables
+  var w = 900, h = 500;
+
+  var container = d3.select("body") //get the <body> element from the DOM
+      .append("svg") //put a new svg in the body
+      .attr("width", w) //assign the width
+      .attr("height", h) //assign the height
+      .attr("class", "container") //always assign a class (as the block name) for styling and future selection
+      .style("background-color", "rgba(0,0,0,0.2)"); //only put a semicolon at the end of the block!
+
+      //Example 1.8 line 1...innerRect block
+  var innerRect = container.append("rect")
+      .datum(400) //a single value is a DATUM
+      .attr("width", function(d){ //rectangle width
+          return d * 2; //400 * 2 = 800
+      })
+      .attr("height", function(d){ //rectangle height
+          return d; //400
+      })
+      .attr("class", "innerRect") //class name
+      .attr("x", 50) //position from left on the x (horizontal) axis
+      .attr("y", 50) //position from top on the y (vertical) axis
+      .style("fill", "#FFFFFF"); //fill color
+
+//Example 2.3 line 1
+var cityPop = [
+    {
+        city: 'Memphis',
+        population: 653450
+    },
+    {
+        city: 'New Orleans',
+        population: 378715
+    },
+    {
+        city: 'St Louis',
+        population: 318416
+    },
+    {
+        city: 'Chicago',
+        population: 2719000
+    }
+];
+
+//Example 2.6 line 3
+var circles = container.selectAll(".circles") //create an empty selection
+    .data(cityPop) //here we feed in an array
+    .enter() //one of the great mysteries of the universe
+    .append("circle") //inspect the HTML--holy crap, there's some circles there
+    .attr("class", "circles")
+    .attr("id", function(d){
+        return d.city;
+    })
+    .attr("r", function(d){
+        //calculate the radius based on population value as circle area
+        var area = d.population * 0.01;
+        return Math.sqrt(area/Math.PI);
+    })
+    .attr("cx", function(d, i){
+        //use the index to place each circle horizontally
+        return 90 + (i * 180);
+    })
+    .attr("cy", function(d){
+        //subtract value from 450 to "grow" circles up from the bottom instead of down from the top of the SVG
+        return 450 - (d.population * 0.0005);
+    });
+
+};
